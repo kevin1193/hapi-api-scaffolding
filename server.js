2992afcb5fb91a0a5057e64180036733b3ffaecb
kevin <kev.abestilla@gmail.com>
2016-07-17 16:53:44 +0800

    Fix in host

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
+    host: 'https://blooming-woodland-87797.herokuapp.com',
     port: 8080
 });
 
