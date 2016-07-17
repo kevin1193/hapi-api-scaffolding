5f573500a8fb9efdfe75958b40ee9f145fa520e8
kevin <kev.abestilla@gmail.com>
2016-07-17 17:44:09 +0800

    Experiment

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const Hapi = require('hapi');
 
 // Create a server with a host and port
 var server = new Hapi.Server();
-server.connection({ port: 4000, labels: ['api'] });
+server.connection({ port: 8080, labels: ['api'] });
 var apiServer = server.select('api');
 // Add the route
 apiServer.route({
     method: 'GET',
     path:'/hello',
     handler: function (request, reply) {
-      
+
         return reply('hello world');
     }
 });
