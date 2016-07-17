33434f0e1fd8167660112f1f3e879010f3bf9579
kevin <kev.abestilla@gmail.com>
2016-07-17 17:14:05 +0800

    Fix

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
-    host: 'localhost',
     port: (+process.env.PORT, '3000')
 });
 
